10e8ae7d13eca4f0039bfbcdcb528af5d736852b
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-05-19 16:46:41 +0300

    Add project files.

---
+++ ConsoleApp38/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ConsoleApp38
+{
+
+    public class Gera
+    {
+        public int a;
+    }
+
+    internal class Program
+    {
+
+
+
+        static void Main(string[] args)
+        {
+            Gera gera1 = new Gera();
+            gera1.a = 12;
+
+
+
+
+        }
+
+
+
+
+
+    }
+}
+
